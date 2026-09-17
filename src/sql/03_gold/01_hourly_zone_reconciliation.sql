69ca4737b5df26d74f6dadc195cf26887254922f
Shiena Marie <arico.smn@gmail.com>
2026-09-18 02:18:35 +0800

    chore(src): remove placeholder 01_hourly_zone_reconciliation.sql

--- src/sql/03_gold/01_hourly_zone_reconciliation.sql
+++
@@ -1,3 +0,0 @@
--- TEMPLATE: enable after gold_mobility_hourly_zone is implemented.
--- Validate uniqueness of pickup_hour_local + pu_location_id and reconcile the
--- sum of trip_count to eligible Silver trips.
